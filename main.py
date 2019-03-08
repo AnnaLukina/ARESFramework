901b7e6d17f14aaecd27daf07becf41a9d337fec
Anna Lukina <lukina.a.u@gmail.com>
2019-03-09 01:54:45 +1100

    full version but buggy

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import numpy as np
 import copy
 
-class parameters:
+class Parameters:
     def __init__(self):
+        self.delta_t = 1.0
+
         #flock parameters
-        self.number_of_agents = 7
+        self.number_of_agents = 5
         self.bounding_box = 3.0
-        self.number_of_time_steps = 100
+        self.number_of_time_steps = 10
         self.random_upper_bound = 2*np.pi
         self.random_lower_bound = 0.0
 
         #swarm parameters
-        self.cognitive_coefficient = 1 / 2
-        self.social_coefficient = 1 / 2
+        self.cognitive_coefficient = 0.5
+        self.social_coefficient = 0.5
         self.inertia_max = 1.0
         self.inertia_min = 0.1
         self.inertia = self.inertia_min
-        self.number_of_swarm_steps = 100
-        self.number_of_particles = 100
+        self.number_of_swarm_steps = 10
+        self.number_of_particles = 10
 
 class Acceleration:
 
@@ -83,7 +85,7 @@ class Particle:
         self.elements = [ParticleElement(parameters=parameters) for i in range(parameters.number_of_agents)]
 
         self.personal_best_elements = copy.deepcopy(self.elements)
-        self.personal_best_objective = computeObjectiveForParticle(flock=flock, particle=self)
+        self.personal_best_objective_value = computeObjectiveForParticle(parameters=parameters, flock=flock, particle=self)
 
 
     def evolve(self, parameters, flock, reference_particle):
@@ -94,37 +96,150 @@ class Particle:
         self.updatePersonalBestElements(flock=flock, candidate_elements=self.elements)
 
     def updatePersonalBestElements(self, flock, candidate_elements):
-        candidate_objective_value = computeObjectiveForParticle(flock=flock, particle=self)
-        if candidate_objective_value < self.global_best_objective_value:
+        candidate_objective_value = computeObjectiveForParticle(parameters=parameters, flock=flock, particle=self)
+        if candidate_objective_value < self.personal_best_objective_value:
             self.personal_best_elements = copy.deepcopy(candidate_elements)
-            self.global_best_objective_value = candidate_objective_value
+            self.personal_best_objective_value = candidate_objective_value
+
+    def getAcceleration(self, agent_i):
+        return self.elements[agent_i].acceleration
 
 class Swarm:
     def __init__(self, parameters, flock):
         self.particles = [Particle(parameters=parameters, flock=flock) for i in range(parameters.number_of_particles)]
 
-        p = min(self.particles, key=lambda p: computeObjectiveForParticle(flock=flock, particle=p))
+        p = min(self.particles, key=lambda p: computeObjectiveForParticle(parameters=parameters, flock=flock, particle=p))
         self.global_best_particle = copy.deepcopy(p)
-        self.global_best_objective_value = computeObjectiveForParticle(flock=flock, particle=self.global_best_particle)
+        self.global_best_objective_value = computeObjectiveForParticle(parameters=parameters, flock=flock, particle=self.global_best_particle)
+
+        self.inertia_counter = 0
 
     def evolveParticles(self, parameters, flock):
         for particle in self.particles:
             particle.evolve(parameters=parameters, flock=flock, reference_particle=self.global_best_particle)
-            self.updateGlobalBestParticle(flock=flock, candidate_particle=particle)
+            self.updateGlobalInformation(parameters=parameters, flock=flock, candidate_particle=particle)
 
-    def updateGlobalBestParticle(self, flock, candidate_particle):
-        candidate_objective_value = computeObjectiveForParticle(flock=flock, particle=candidate_particle)
+    def updateGlobalInformation(self, parameters, flock, candidate_particle):
+        #update global best partical
+        candidate_objective_value = computeObjectiveForParticle(parameters=parameters, flock=flock, particle=candidate_particle)
         if candidate_objective_value < self.global_best_objective_value:
             self.global_best_particle = copy.deepcopy(candidate_particle)
             self.global_best_objective_value = candidate_objective_value
+            self.inertia_counter = max(self.inertia_counter - 1, 0)
+        else:
+            self.inertia_counter += 1
+
+        # update inertia
+        if self.inertia_counter < 2:
+            self.inertia_counter = max(parameters.inertia_min, min(parameters.inertia_max, 2.0 * parameters.inertia))
+        else:
+            if self.inertia_counter > 5:
+                parameters.inertia = max(parameters.inertia_min, min(parameters.inertia_max, 0.5 * parameters.inertia))
+
+
+def v_matching(vx, vy, num):
+    assert(len(vx) > 0)
+    assert(len(vy) > 0)
+
+    sum = 0.0
+    diff = 0.0
+    for i in range(0, num):
+        for j in range(i+1, num):
+            diff = np.linalg.norm([vx[i] - vx[j], vy[i] - vy[j]]) / (np.linalg.norm([vx[i], vy[i]]) + np.linalg.norm([vx[j], vy[j]]))
+            sum += diff * diff
+    return sum
+
+def computeObjective(parameters, flock):
+    benefit = 0
+    la = 0.5 - np.pi / 8.0
+    w = 1.0
+    d0 = 1.0
+    u_sigma1 = 5.0
+    u_sigma2 = 5.0
+    d_sigma1 = 1.0 / 0.3
+    d_sigma2 = 1.0 / 0.7
+    heading = np.pi/2
+    headFit = 0
+
+    x = np.array([[bird.x_position, bird.y_position] for bird in flock.birds])
+    dx = np.array([[bird.x_velocity, bird.y_velocity] for bird in flock.birds])
+    obstacle = 0.0
+
+    for i in range(len(flock.birds)):
+
+        #x = [bird.x_position, bird.y_position] #x = agent.model.X
+        #dx = [bird.x_velocity, bird.y_velocity] #dx = agent.model.dX
+        A = dx[i,0]
+        B = dx[i,1]
+        C = -dx[i,1] * x[i,1] - dx[i,0] * x[i,0]
+        ub_j = 0
+
+        # heading direction
+        if (np.math.atan2(dx[i,0], dx[i,1]) != heading):
+            headFit += abs(np.math.atan2(dx[i,0], dx[i,1]) - heading)
+
+        for  j in range(len(flock.birds)):
+            if i != j and np.linalg.norm(x[j,:]) < np.linalg.norm(x[i,:]):
+
+                if (dx[i,0] == 0.0):
+                    px = x[j,0]
+                    py = x[i,1]
+                else:
+                    if (dx[i,1] == 0.0):
+                        px = x[i,0]
+                        py = x[j,1]
+                    else:
+                        k = -dx[i,0] / dx[i,1]
+                        px = (k * x[i,0] + x[j,0] / k + x[j,1] - x[i,1]) / (k + 1.0 / k)
+                        py = -1.0 / k * (px - x[j,0]) + x[j,1]
+
+                side = A * x[j,0] + B * x[j,1] + C
+                h_dis = np.linalg.norm([px - x[i,0], py - x[i,1]])
+                v_dis = abs(side) / np.linalg.norm([A, B])
+
+                sm = np.math.erf((h_dis - (w - la)) * np.sqrt(2.0) * 8.0)
+                dot_prod = (dx[i,0] * dx[j,0] + dx[i,1] * dx[j,1]) / (np.linalg.norm([dx[i,0], dx[i,1]]) * np.linalg.norm([dx[j,0], dx[j,1]]))
+
+                from scipy.stats import multivariate_normal
+                if (side > 0.0 and h_dis >= w - la):
+                    ub_j += dot_prod * sm * multivariate_normal.pdf([h_dis - (2.0 * w - la), v_dis - d0], [0,0], [u_sigma1, u_sigma2])
+                else:
+                    if (side >= 0.0 and h_dis < w - la):
+                        ub_j += sm * multivariate_normal.pdf([h_dis, v_dis], [0,0], [d_sigma1, d_sigma2])
+
+                angle = np.pi / 6.0
+
+                blocks = [[0.0, 0.0] for i in range(parameters.number_of_agents)]
+
+                if (side >= 0.0 and (h_dis < w or (h_dis - w) / v_dis < np.tan(angle))):
+                  blocks[j][0] = np.arctan(v_dis / (h_dis + w))
+                  blocks[j][1] = np.arctan2(v_dis, h_dis - w)
+                  if (blocks[j][0] < np.pi / 2.0 - angle):
+                    blocks[j][0] = np.pi / 2.0 - angle
+                  if (blocks[j][1] > np.pi / 2.0 + angle):
+                    blocks[j][1] = np.pi / 2.0 + angle
+                  obstacle += (blocks[j][1] - blocks[j][0]) / (angle)
+
+
+
+
+
+        if (ub_j < 1.0):
+            benefit += ub_j
+        else:
+            benefit += 1.0
+
+
+    fit = pow(len(flock.birds) - 1.0 - benefit, 2) + v_matching(vx=dx[:,0], vy=dx[:,1], num=parameters.number_of_agents) + pow(obstacle, 2)# + pow(headFit/len(agents), 2)
+    return fit
 
 
 #compute the objective value which would be obtained if the flock moved one time step with respect to the particle
-def computeObjectiveForParticle(flock, particle):
-    assert(1==2)
-    return None
-#todo
-
+def computeObjectiveForParticle(parameters, flock, particle):
+    initial_flock = copy.deepcopy(flock)
+    initial_flock.updatePositions(parameters=parameters, particle=particle)
+    objective_value = computeObjective(parameters=parameters, flock=initial_flock)
+    return objective_value
 
 class Bird:
     def __init__(self, x, y, dx, dy, ddx, ddy):
@@ -136,6 +251,25 @@ class Bird:
 
         self.x_acceleration = ddx
         self.y_acceleration = ddy
+
+    #note that acceleration might change due to trimming :o
+    def move(self, parameters, acceleration):
+        # trim - reduce the acceleration if it is too big
+        norm = np.linalg.norm([self.x_velocity, self.y_velocity])
+        if acceleration.magnitude >= norm:
+            acceleration.magnitude = norm
+
+        #convert the received acceleration to x y coordinates
+        self.x_acceleration = acceleration.magnitude * np.cos(acceleration.angle)
+        self.y_acceleration = acceleration.magnitude * np.sin(acceleration.angle)
+
+        #update velocities
+        self.x_velocity += (self.x_acceleration * parameters.delta_t)
+        self.y_velocity += (self.y_acceleration * parameters.delta_t)
+
+        #update positions
+        self.x_position += (self.x_velocity * parameters.delta_t)
+        self.y_position += (self.y_velocity * parameters.delta_t)
 
 def randomBird(parameters):
     return Bird(x=np.random.uniform(0, parameters.bounding_box),
@@ -149,21 +283,77 @@ class Flock:
     def __init__(self, parameters):
         self.birds = [randomBird(parameters=parameters) for i in range(parameters.number_of_agents)]
 
+    def updatePositions(self, parameters, particle):
+        assert(len(particle.elements) == parameters.number_of_agents)
+
+        for agent_i in range(parameters.number_of_agents):
+            self.birds[agent_i].move(parameters=parameters, acceleration=particle.getAcceleration(agent_i))
+
 def generateRandomFlock(parameters):
     return Flock(parameters=parameters)
 
-def temp(parameters):
+def tempName(parameters):
     current_flock = generateRandomFlock(parameters=parameters)
+    current_objective_value = computeObjective(parameters=parameters, flock=current_flock)
     best_particles = []
+
     for time_i in range(parameters.number_of_time_steps):
-        best_particle = computeBestParticle(flock=current_flock, parameters=parameters)
-        updatePositions(current_flock, best_particle)
+        #todo properly include the horizon approach and levels
+        print(time_i)
+        print("current objective value: {}".format(current_objective_value))
+        (best_particle, best_objective_value) = computeBestParticle(flock=current_flock, parameters=parameters)
+        hehe = 0
+        while best_objective_value > current_objective_value:
+            (best_particle, best_objective_value) = computeBestParticle(flock=current_flock, parameters=parameters)
+            hehe += 1
+
+            if hehe > 10:
+                print("Sadness!")
+                break
+
+        current_flock.updatePositions(parameters=parameters, particle=best_particle)
+        current_objective_value = best_objective_value
         best_particles.append(best_particle)
+
+        #TODO FIX FOR THE BIRDS LOOOOOOOOOOOOOOOOOOL
+        #parameters.random_upper_bound
+        #options[b].ub[j] = delta * norm(info[b].cvx[k], info[b].cvy[k]);
+        #printf("options_ub: %f\t, ind: %d\t, b: %d\n", options[b].ub[j], k, b);
+        #j++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        import matplotlib.pyplot as plt
+
+        X = np.array([[bird.x_position, bird.y_position] for bird in current_flock.birds])
+        dX = np.array([[bird.x_velocity, bird.y_velocity] for bird in current_flock.birds])
+
+        Num = parameters.number_of_agents
+        fig, ax = plt.subplots()
+        ax.plot(X[:, 0], X[:, 1], 'ro')
+        N = [x for x in range(0, Num)]
+        for i, txt in enumerate(N):
+            ax.annotate(txt, (X[-i - 1, 0], X[-i - 1, 1]))
+        ax.quiver(X[:, 0], X[:, 1], dX[:, 0], dX[:, 1])
+        plt.show()
 
 #computes the best particle for the current position of the flock
 #uses particle swarm optimisation
 def computeBestParticle(flock, parameters):
-    swarm = Swarm(parameters=parameters) #for now, the swarm is randomly generated
+    swarm = Swarm(parameters=parameters, flock=flock) #for now, the swarm is randomly generated
     for i in range(parameters.number_of_swarm_steps):
         swarm.evolveParticles(parameters=parameters, flock=flock) #note that this might modify the best particle of the swarm
-    return swarm.best_particle
+    return (swarm.global_best_particle, swarm.global_best_objective_value)
+
+parameters = Parameters()
+tempName(parameters=parameters)
